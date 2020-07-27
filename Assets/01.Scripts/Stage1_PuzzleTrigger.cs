f68b54bb7beb5f3f243fd9577ef62fe162748271
FFLINA <hyunjin032@daum.net>
2020-07-28 08:51:30 +0900

    0728
    폴더변경

---
+++ Assets/01.Scripts/Stage1_PuzzleTrigger.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage1_PuzzleTrigger : MonoBehaviour
+{
+    string answer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (gameObject.name)
+        {
+            case "Stage 1 Trigger_Red":
+                answer = "Stage 1 Item_Red";
+                break;
+
+            case "Stage 1 Trigger_Green":
+                answer = "Stage 1 Item_Green";
+                break;
+        }
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    string itemName;
+    private void OnTriggerEnter(Collider item)
+    {
+        itemName = item.gameObject.name;
+        if (itemName == answer)
+        {
+            Invoke("Stage1_SetTrigger", 2f);
+        }
+    }
+
+    private void OnTriggerExit(Collider item)
+    {
+        if (itemName == answer)
+        {
+            CancelInvoke("Stage1_SetTrigger");
+            //PuzzleManager.Instance.StageOneOffTrigger();
+        }
+    }
+
+    void Stage1_SetTrigger()
+    {
+        PuzzleManager.Instance.StageOneOnTrigger();
+    }
+}
